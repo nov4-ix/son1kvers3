18c9fad54b09fb32d69d0bab2d4773ebe9564734
nov4-ix <nov4.ix@gmail.com>
2026-01-28 03:38:57 -0600

    chore: archive legacy deployment artifacts from repo (remove from git tracking)

--- Sub-Son1k-2.3/_legacy_archive/unused_apps/the-generator-nextjs/__mocks__/fileMock.js
+++
@@ -1,2 +0,0 @@
-// Mock para archivos estáticos como imágenes
-export default 'test-file-stub';
--- Sub-Son1k-2.3/_legacy_archive/unused_apps/the-generator-nextjs/components/ClientAuthProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { AuthProvider } from '../context/AuthContext';
-
-export default function ClientAuthProvider({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
-
